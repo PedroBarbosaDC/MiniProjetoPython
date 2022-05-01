bd30209bac267f56b732b2f37f175a3781f655d4
Pedro Barbosa <pedaocds@gmail.com>
2022-05-01 20:48:48 -0300

    Update MeuArquivo.py

--- MeuArquivo.py
+++ MeuArquivo.py
@@ -1,3 +1,4 @@
+from itertools import groupby
 import pandas as pd
 
 # importar base de dados
@@ -5,4 +6,22 @@ tabela_vendas = pd.read_excel('Vendas.xlsx')
 
 # visualizar a base de dados
 pd.set_option('display.max_columns', None) # mostrar todas as colunas da tabela
-print(tabela_vendas)
+#print(tabela_vendas[['ID Loja','Valor Final']]) # filtrar algumas colunas da tabela
+
+# faturamento por loja
+# agrupar coluna somando  valores
+faturamento = tabela_vendas[['ID Loja','Valor Final']].groupby('ID Loja').sum()
+print(faturamento)
+
+
+
+
+
+
+
+
+
+
+
+
+
