b09568335a53cd0574f25fc9d9ad00ac043e6749
Pedro Barbosa <pedaocds@gmail.com>
2022-05-01 18:39:54 -0300

    ImportadoTabela

--- MeuArquivo.py
+++ MeuArquivo.py
@@ -2,4 +2,7 @@ import pandas as pd
 
 # importar base de dados
 tabela_vendas = pd.read_excel('Vendas.xlsx') 
+
+# visualizar a base de dados
+pd.set_option('display.max_columns', None) # mostrar todas as colunas da tabela
 print(tabela_vendas)
